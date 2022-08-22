62a3a2b80c0d74d65d1f8326150252b3f3e0571c
numangit <abdulnumanjob@gmail.com>
2022-08-22 08:55:23 +0600

    event listeners added in the budget buttons

--- js/budget.js
+++ js/budget.js
@@ -14,4 +14,32 @@ function setElementById(elementId, value) {
 }
 
 let playerSelectedCount;
+let totalPlayerExpense;
 
+// event listener for calculate button
+document.getElementById('calculate-btn').addEventListener('click', function () {
+
+    const perPlayerExpense = getInputFieldValueById("per-player-field");
+
+    // Error handling: to check if the input is a number and if its less than 0.
+    if (isNaN(perPlayerExpense) || perPlayerExpense < 0) {
+        alert('Please enter only valid number');
+    } else {
+        totalPlayerExpense = perPlayerExpense * 5;
+        setElementById("player-expense", totalPlayerExpense);
+    }
+})
+
+// event listener for calculate total button
+document.getElementById('calculate-total-btn').addEventListener('click', function () {
+    const managerExpense = getInputFieldValueById('manager-expense-field');
+    const coachExpense = getInputFieldValueById('coach-expense-field');
+
+    // Error handling: to check if the input is a number and if its less than 0.
+    if (isNaN(managerExpense) || isNaN(coachExpense) || managerExpense < 0 || coachExpense < 0) {
+        alert('Please enter only valid number');
+    } else {
+        const totalExpense = managerExpense + coachExpense + totalPlayerExpense;
+        setElementById('total-expense', totalExpense);
+    }
+})
